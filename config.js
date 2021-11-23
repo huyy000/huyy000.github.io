8cd361ed19ff08eece449661773e8a00a36fe439
huyy000 <huytran982005@gmail.com>
2021-11-23 14:46:33 +0700

    New1

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Chúc mừng sinh nhật bạn Táo",
+    introTitle: 'Hellu Bạn Táo 🍎🍎🍎',
+    introDesc: `Hôm nay là 29/11 cũng là sinh nhật bạn =)))`,
+    btnIntro: 'OK',
+    title: 'Happy Birth Day 🎂🎂🎂',
+    desc: `Chúc bạn sinh nhật đầy sự yêu thương, tuổi mới thêm nhiều hạnh phúc mới, niềm vui mới
+    Luôn luôn vui vẻ, không được quạo, hạnh phúc và nhận được nhiều quà nhé.`,
+    btnYes: 'Okk',
+    btnNo: 'Khôngg',
+    question: 'Cám ơn đi nàooo',
+    btnReply: 'Gửi đi nào =))',
+    reply: 'Cám ơn nhiềuu!!',
+    mess: 'Cám ơn bạn nhìu :))',
+    messDesc: 'Tóm lại là sinh nhật vui vẻ nhé bạn Táo',
+    btnAccept: 'Oke ❤️',
+    messLink: 'https://www.facebook.com/messages/t/100054350271388' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
